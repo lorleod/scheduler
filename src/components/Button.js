50cfa4801ac01d3e9bd35cc080a480518773fdac
Taylor McLeod <Taylor@xyloMac.local>
2022-06-14 17:15:00 -0700

    completion of building a button and having it conditionally render styles based on state.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = classNames(
+    "button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+    <button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
 }
