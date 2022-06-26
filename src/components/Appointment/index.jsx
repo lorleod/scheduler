6009c12b8ba2b840a6267c6ae70690ca9bc799c2
Taylor McLeod <Taylor@xyloMac.hitronhub.home>
2022-06-25 22:24:54 -0700

    confirm delete is working

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -16,6 +16,7 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -47,7 +48,7 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           interviewers={props.interviewers}
-          onDelete={deleteInterview}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
       {mode === CREATE && (
@@ -62,6 +63,13 @@ export default function Appointment(props) {
       )}
       {mode === DELETING && (
         <Status message="Deleting" />
+      )}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you want to delete?"
+          onCancel={() => back()}
+          onConfirm={deleteInterview}
+        />
       )}
     </article>
   );
